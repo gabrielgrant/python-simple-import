01b6a13326c73b1654aaf5c47359fc2966115de2
Gabriel Grant <g@briel.ca>
2011-11-16 15:12:39 -0800

    add author_email and bump version

--- setup.py
+++ setup.py
@@ -2,8 +2,9 @@ from setuptools import setup
 
 setup(
     name='simple-import',
-    version='0.1.0',
+    version='0.1.1',
     author='Gabriel Grant',
+    author_email='g@briel.ca',
     py_modules=['simple_import'],
     license='LGPL',
     long_description=open('README').read(),
